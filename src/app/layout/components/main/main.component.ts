7540635a0d788f0d14a55331046e84814a6c8dda
Edilson Nantes <nantes.junior@gmail.com>
2025-03-03 05:44:03 -0300

    feat: Adjusts and add styles on layout components

--- src/app/layout/components/main/main.component.ts
+++ src/app/layout/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from "../topbar/topbar.component";
 
 @Component({
   selector: 'app-main',
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, TopbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  menuItems = [
+    { label: 'Produtos', icon: 'pi pi-fw pi-id-card', link: '/produtos' },
+    { label: 'Fornecedores', link: '/fornecedores' },
+    { label: 'Notas Fiscais', link: '/notas-fiscais' }
+  ]
 }
