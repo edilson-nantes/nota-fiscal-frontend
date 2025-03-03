4e2b884e9ac23b985b46c90d4202167b4155bf5a
Edilson Nantes <nantes.junior@gmail.com>
2025-03-03 04:15:49 -0300

    feat: Initial layout setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './layout/components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            
+        ]
+    },
+];
